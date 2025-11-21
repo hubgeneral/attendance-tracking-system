939a33ab5c4610ee808ff107af7fb9297553e314
Janet <janetteakua@gmail.com>
2025-11-21 12:23:08 +0000

    Implement Request List & Unconfirmed Modification Connections

--- src/generated/graphql.ts
+++ src/generated/graphql.ts
@@ -902,6 +902,22 @@ export type GeofenceClockOutMutationVariables = Exact<{
 
 export type GeofenceClockOutMutation = { __typename?: 'Mutation', geofenceClockOut: string };
 
+export type MakeARequestMutationVariables = Exact<{
+  userId: Scalars['Int']['input'];
+  reason: Scalars['String']['input'];
+}>;
+
+
+export type MakeARequestMutation = { __typename?: 'Mutation', createRequestLog: string };
+
+export type UpdateRequestMutationVariables = Exact<{
+  requestId: Scalars['Int']['input'];
+  reason: Scalars['String']['input'];
+}>;
+
+
+export type UpdateRequestMutation = { __typename?: 'Mutation', updateRequestLog: string };
+
 export type ResetPasswordMutationVariables = Exact<{
   userName: Scalars['String']['input'];
   newPassword: Scalars['String']['input'];
@@ -927,6 +943,13 @@ export type GetAttendanceByUserIdQueryVariables = Exact<{
 
 
 export type GetAttendanceByUserIdQuery = { __typename?: 'Query', attendanceByUserId: { __typename?: 'Attendance', clockIn?: any | null, clockOut?: any | null, currentDate?: any | null }[] };
+
+export type GetRequestsByUserIdQueryVariables = Exact<{
+  id: Scalars['Int']['input'];
+}>;
+
+
+export type GetRequestsByUserIdQuery = { __typename?: 'Query', requestLogsByUserId: Array<{ __typename?: 'RequestLog', id: number, reason?: string | null, approvalStatus?: string | null }> };
 
 export type GetUserByIdQueryVariables = Exact<{
   id: Scalars['Int']['input'];
@@ -1045,6 +1068,70 @@ export function useGeofenceClockOutMutation(baseOptions?: Apollo.MutationHookOpt
 export type GeofenceClockOutMutationHookResult = ReturnType<typeof useGeofenceClockOutMutation>;
 export type GeofenceClockOutMutationResult = Apollo.MutationResult<GeofenceClockOutMutation>;
 export type GeofenceClockOutMutationOptions = Apollo.BaseMutationOptions<GeofenceClockOutMutation, GeofenceClockOutMutationVariables>;
+export const MakeARequestDocument = gql`
+    mutation MakeARequest($userId: Int!, $reason: String!) {
+  createRequestLog(userid: $userId, reason: $reason)
+}
+    `;
+export type MakeARequestMutationFn = Apollo.MutationFunction<MakeARequestMutation, MakeARequestMutationVariables>;
+
+/**
+ * __useMakeARequestMutation__
+ *
+ * To run a mutation, you first call `useMakeARequestMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useMakeARequestMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [makeARequestMutation, { data, loading, error }] = useMakeARequestMutation({
+ *   variables: {
+ *      userId: // value for 'userId'
+ *      reason: // value for 'reason'
+ *   },
+ * });
+ */
+export function useMakeARequestMutation(baseOptions?: Apollo.MutationHookOptions<MakeARequestMutation, MakeARequestMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<MakeARequestMutation, MakeARequestMutationVariables>(MakeARequestDocument, options);
+      }
+export type MakeARequestMutationHookResult = ReturnType<typeof useMakeARequestMutation>;
+export type MakeARequestMutationResult = Apollo.MutationResult<MakeARequestMutation>;
+export type MakeARequestMutationOptions = Apollo.BaseMutationOptions<MakeARequestMutation, MakeARequestMutationVariables>;
+export const UpdateRequestDocument = gql`
+    mutation UpdateRequest($requestId: Int!, $reason: String!) {
+  updateRequestLog(requestId: $requestId, reason: $reason)
+}
+    `;
+export type UpdateRequestMutationFn = Apollo.MutationFunction<UpdateRequestMutation, UpdateRequestMutationVariables>;
+
+/**
+ * __useUpdateRequestMutation__
+ *
+ * To run a mutation, you first call `useUpdateRequestMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useUpdateRequestMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [updateRequestMutation, { data, loading, error }] = useUpdateRequestMutation({
+ *   variables: {
+ *      requestId: // value for 'requestId'
+ *      reason: // value for 'reason'
+ *   },
+ * });
+ */
+export function useUpdateRequestMutation(baseOptions?: Apollo.MutationHookOptions<UpdateRequestMutation, UpdateRequestMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<UpdateRequestMutation, UpdateRequestMutationVariables>(UpdateRequestDocument, options);
+      }
+export type UpdateRequestMutationHookResult = ReturnType<typeof useUpdateRequestMutation>;
+export type UpdateRequestMutationResult = Apollo.MutationResult<UpdateRequestMutation>;
+export type UpdateRequestMutationOptions = Apollo.BaseMutationOptions<UpdateRequestMutation, UpdateRequestMutationVariables>;
 export const ResetPasswordDocument = gql`
     mutation ResetPassword($userName: String!, $newPassword: String!, $token: String!) {
   resetPassword(username: $userName, token: $token, password: $newPassword) {
