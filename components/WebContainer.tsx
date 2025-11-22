0753a5109e15e0177864ea74d0b5161e94ccaaa9
illusionalgod <justinsane28@protonmail.com>
2025-11-22 01:47:17 -0800

    fix

--- components/WebContainer.tsx
+++ components/WebContainer.tsx
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
   },
   webContent: {
     width: "100%",
-    maxWidth: 490, 
+    maxWidth: "48%", 
     flex: 1,
     backgroundColor: "#f2f5f7",
     shadowColor: "#000",
